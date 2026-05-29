95412b91ca7664cc4411de934743902054eb8d0a
Kpemini Delali Beatrice <kpeminibeatrice18@gmail.com>
2026-05-29 16:40:52 +0000

    Submit assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Beatrice Delali Kpemini
+# STUDENT ID:  2425402514
+# Email: 2425402514@gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Beatrice Delali Kpemini, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+     # Print my initials
 
+    print()
+    print("...BDK")
+    print()
 
+     # Large letters go here
+    print("...BBBBBBBB............DDDDDDDD............KKKK........KKK..")
+    print("...BBBBBBBBB...........DDDDDDDDD...........KKKK.......KKK...")
+    print("...BB.......BBB........DD.......DDD........KKKK......KKK....")
+    print("...BB........BB........DD........DD........KKKK.....KKK.....")
+    print("...BBBBBBBBBB..........DD........DD........KKKKKKKK.........")
+    print("...BBBBBBBBBB..........DD........DD........KKKKKKKK.........")
+    print("...BB........BB........DD........DD........KKKK.....KKK.....")
+    print("...BB.......BBB........DD.......DDD........KKKK......KKK....")
+    print("...BBBBBBBBB......**...DDDDDDDDD......**...KKKK.......KKK.**")
+    print("...BBBBBBBB.......**...DDDDDDDD.......**...KKKK........KKK**")
+    print()
 
 main()
